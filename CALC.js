2605063435d4b3739fbb98ba4cdead22ef19a7cd
atomu21 <98013520+atomu21@users.noreply.github.com>
2022-03-01 20:51:14 +0900

    UPdate CALC.js
    符号の連続入力できないように修正及び、その他の改善。

--- CALC.js
+++ CALC.js
@@ -1,14 +1,116 @@
-function calc(cmd){
-    const element = document.getElementById('display')
-    const value = element.value
+'use strict'
+{
+  const num_btn = document.querySelectorAll('.num_btn');
+  const output_total = document.getElementById('output_total');
+  let total = 0;
+  let state = 'start';  
+  let mode = 'integer_mode';
 
-    if(cmd === '='){
-        element.value = eval(value)
-    }else if(cmd === 'AC'){
-        element.value = '0'
-    }else if(value === '0') {
-        element.value = cmd
+  const one_nine = document.querySelectorAll('.one_nine');
+  one_nine.forEach(index => {     
+    index.addEventListener('click', () => {
+      if(state === 'start') {
+        total = index.dataset.indexId;         
+      }else if(state === 'numeric_state'||state === 'operator_state'){
+        total += index.dataset.indexId;
+      }else if(state === 'finish') {
+        return;
+      }     
+      output_total.textContent = total;
+      state = 'numeric_state'
+      changeOutput()
+    })   
+  })
+
+  const zero = document.getElementById('zero');
+  zero.addEventListener('click', () => {
+    if(state === 'numeric_state') {
+      total += zero.dataset.indexId;
     }else{
-        element.value += cmd
+      return;
+    }      
+    output_total.textContent = total;
+    changeOutput()
+    state = 'numeric_state'
+  }) 
+  
+  const w_zero = document.getElementById('w_zero');
+  w_zero.addEventListener('click', () => {
+    if(state === 'numeric_state') {
+      total += w_zero.dataset.indexId;
+    }else{
+      return;
     }
+    output_total.textContent = total;
+    changeOutput()
+    state = 'numeric_state'
+  })
+
+  const point = document.getElementById('point');
+  point.addEventListener('click', () => {
+    console.log(point.dataset.indexId)
+    if(mode === 'decimal_mode'){
+      return;
+    }      
+    if(state === 'start'){
+      total = 0;
+    }else if(state === 'operator_state'){
+      total += 0;
+    }else if(state === 'finish'){
+      return;
+    }
+    total += point.dataset.indexId;
+    output_total.textContent = total;
+    state = 'numeric_state'
+    mode = 'decimal_mode';
+    changeOutput()
+  })
+
+  const operator = document.querySelectorAll('.operator');
+  operator.forEach(index => {     
+    index.addEventListener('click', () => {
+      if(state === 'start') {
+        return;
+      }else if(state === 'numeric_state'){
+        total += index.dataset.indexId;
+      }else if(state ==='operator_state') {
+        total = total.slice(0, -1)
+        total += index.dataset.indexId;
+      }else if(state === 'finish'){
+        total = output_total.textContent;
+        total += index.dataset.indexId;
+        output_total.textContent = 0
+      }
+      output_total.textContent = total;
+      state = 'operator_state'
+      mode ='integer_mode'
+      changeOutput()
+    })
+  })
+
+  const equal_btn = document.getElementById('equal_btn');
+  equal_btn.addEventListener('click',() =>{
+    console.log(eval(total));
+    output_total.textContent = digitNum(eval(total));
+    state = 'finish'
+    mode ='integer_mode'
+    changeOutput()
+  });
+
+  const clear = document.getElementById('clear')
+  clear.addEventListener('click', () => {
+    reset();
+  })
+
+  function reset() {
+    total = 0; 
+    output_total.textContent = 0;
+    mode ='integer_mode'
+    state ='start';
+    changeOutput()
+  }
+
+  function digitNum(num) {
+    return Math.round(num*1000)/1000;
+  }
 }
